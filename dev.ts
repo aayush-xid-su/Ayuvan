960a14ddfa0cfa5f6278874a00bb35ba87ad65e1
aayush_xid_su <ayushxidsu@gmail.com>
2026-02-08 19:31:36 +0530

    old outdated files

--- dev.ts
+++
@@ -1,3 +0,0 @@
-// This line is crucial to load the genkit configuration.
-import '@/ai/genkit';
-import '@/ai/flows/diagnose-plant-disease-and-suggest-remedies.ts';
--- genkit.ts
+++
@@ -1,8 +0,0 @@
-import {genkit} from 'genkit';
-import {googleAI} from '@genkit-ai/google-genai';
-import {firebaseApp} from '@/firebase/config';
-
-export const ai = genkit({
-  plugins: [googleAI(firebaseApp)],
-  model: 'googleai/gemini-pro',
-});
